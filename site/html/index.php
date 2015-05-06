9a1d3ebe75419e68b479fb44ae2502f9b375f7d1
Aldrin Cabuniag <aldrincabuniag@gmail.com>
2015-05-06 09:27:54 -0700

    Added main base files

---
+++ site/html/index.php
@@ -0,0 +1,15 @@
+<html>
+<!DOCTYPE HTML>
+	<head lang="en">
+		<meta charset="utf-8">
+		<title> Black Box </title>
+		<link href="" type="image/x-icon" rel="icon">
+		<link href="" type="image/x-icon" rel="shortcut icon">
+	</head>
+
+	<body>
+		<div id="main-body">
+			<p> Home Page </p>
+		</div>
+	</body>
+</html>
