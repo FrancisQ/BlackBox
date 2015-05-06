f28ff59e464d143c54d2336fb098bbbb0c8d9e3a
Aldrin Cabuniag <aldrincabuniag@gmail.com>
2015-05-06 13:12:43 -0700

    Updated on main site with images and css files

--- site/html/index.php
+++ site/html/index.php
@@ -5,11 +5,42 @@
 		<title> Black Box </title>
 		<link href="" type="image/x-icon" rel="icon">
 		<link href="" type="image/x-icon" rel="shortcut icon">
+		
+		<!--Global style sheet-->
+		<link href="../css/base.css" type="text/css" rel="stylesheet">
+		
+		<!--Specific site style sheet-->
+		<link href="../css/main.css" type="text/css" rel="stylesheet">
 	</head>
 
 	<body>
-		<div id="main-body">
-			<p> Home Page </p>
+		
+		<div id="wrapper">
+		
+			<!--Nav -->
+			<div id="main-Nav">
+				<div id="nav-cont">
+					<ul>
+						<li>Home</li>
+						<li>Play Blackbox</li>
+						<li>How to</li>
+						<li>About us</li>
+					</ul>
+				</div>
+			</div>
+			
+			<!--Contents Body-->
+			<div id="main-body" class="absolute-center">
+				<img src="../img/BBlogo.png" alt="Blackbox Logo" id="blackbox-Logo" class="absolute-center">
+			</div>
+			
+			
+			<!--Footer -->
+			<div id="main-footer">
+				<div id="nav-footer">
+					<img src="../img/fabse.png" alt="Fabse Games" id="fabseImg">
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
