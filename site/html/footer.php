ffe0b1b1e2573854de4eec383927bc629e31ae95
Aldrin Cabuniag <aldrincabuniag@gmail.com>
2015-05-06 21:41:06 -0700

    How2,Main and game page edits

--- site/html/footer.php
+++ site/html/footer.php
@@ -1,11 +1,7 @@
+<!--Footer -->
 <div id="main-footer">
-	<div id="nav-footer">
-		<ul>
-			<li>Home</li>
-			<li>Play Blackbox</li>
-			<li>How to</li>
-			<li>About us</li>
-		</ul>
-		<p> footer </p>
+	<div id="nav-footer" class="imgCent">
+		<!-- <embed src="../music/theme.mp3" autostart="false" loop="true"> -->
+		<img src="../img/fabse.png" alt="Fabse Games" id="fabseImg">
 	</div>
 </div>
--- site/html/index.php
+++ site/html/index.php
@@ -24,31 +24,37 @@
 		<div id="wrapper">
 		
 			<!--Nav -->
+			<!-- <?php
+				include('nav.php');
+			?> -->
 			<div id="main-Nav">
 				<div id="nav-cont">
 					<ul>
-						<li> <a href="#" >Home</a></li>
-						<li> <a href="#">Play Blackbox</a></li>
-						<li> <a href="#">How to </a></li>
-						<li> <a href="#">About us </a></li>
+						<li> <a href="index.php" >Home</a></li>
+						<li> <a href="blackbox.php">Play Blackbox</a></li>
+						<li> <a href="how-2.php">How to </a></li>
 					</ul>
 				</div>
 			</div>
 			
 			<!--Contents Body-->
 			<div id="main-body">
-				
 				<img src="../img/BBlogo.png" alt="Blackbox Logo" id="blackbox-Logo" class="absolute-center">
 				<img src="../img/blackBoxTextBlack.png" alt="Blackbox Name" class="absolute-center">
 			</div>
 			
 			
 			<!--Footer -->
-			<div id="main-footer">
-				<div id="nav-footer">
+			<!-- <?php
+				include('footer.php');
+			?> -->
+			<div id="main-footer-home">
+				<div id="nav-footer" class="imgCent">
+					<!-- <embed src="../music/theme.mp3" autostart="false" loop="true"> -->
 					<img src="../img/fabse.png" alt="Fabse Games" id="fabseImg">
 				</div>
 			</div>
+			
 		</div>
 	</body>
 </html>
